bdd25fed6c99bdb9dc9e0df05fe3acfd623496d0
AwaleSajil <sa0812@uah.edu>
2026-05-10 17:29:09 -0500

    added experiment sh files

--- experiments/e1_run_baseline.sh
+++ experiments/e1_run_baseline.sh
@@ -1,7 +1,11 @@
 # Train
-torchrun --nproc_per_node=4 ../src/train.py --expirement_number e1
+# torchrun --nproc_per_node=4 ../src/train.py --expirement_number e1
+
+# torchrun --nproc_per_node=4 ../src/train.py --expirement_number e1 --model_name FacebookAI/roberta-base
+
+# torchrun --nproc_per_node=4 ../src/train.py --expirement_number e1 --model_name FacebookAI/roberta-base --resume_checkpoint_path /rhome/sawale/thesis/models/nrows_None__nsrc_None/timestamp_20260505_02-52-31/roberta-base/checkpoints/checkpoint-7000 --resume_run_id 1jahq5s6
 
 # Evaluate
-python ../eval/eval.py --dataset_name nanobeir --json_output_path ../eval/results_json/ --output_dir_plots ../eval/results_plots/ --json_time_path ../eval/results_times/ --emb_info_path ../eval/results_emb_info/
+# python ../eval/eval.py --dataset_name nanobeir --json_output_path ../eval/results_json/ --output_dir_plots ../eval/results_plots/ --json_time_path ../eval/results_times/ --emb_info_path ../eval/results_emb_info/
 
 # Store results
--- experiments/e4_run_bat_mrl.sh
+++ experiments/e4_run_bat_mrl.sh
@@ -1,5 +1,9 @@
 # Train
-torchrun --nproc_per_node=4 ../src/train.py  --expirement_number e4
+# torchrun --nproc_per_node=4 ../src/train.py  --expirement_number e4
+
+# torchrun --nproc_per_node=4 ../src/train.py --expirement_number e4 --model_name FacebookAI/roberta-base
+
+torchrun --nproc_per_node=4 ../src/train.py --expirement_number e4 --model_name microsoft/mpnet-base
 
 # Evaluate
-python ../eval/eval.py --dataset_name nanobeir --json_output_path ../eval/results_json/ --output_dir_plots ../eval/results_plots/ --json_time_path ../eval/results_times/ --emb_info_path ../eval/results_emb_info/
+# python ../eval/eval.py --dataset_name nanobeir --json_output_path ../eval/results_json/ --output_dir_plots ../eval/results_plots/ --json_time_path ../eval/results_times/ --emb_info_path ../eval/results_emb_info/
